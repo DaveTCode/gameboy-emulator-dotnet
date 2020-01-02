f2caf24833b2f63c1ff8d77455357336e8cfc2a0
David Tyler <david.tyler@metaswitch.com>
2020-01-02 16:37:53 +0000

    Added test for call/return to validate function calls

---
+++ Gameboy.VM.Cpu.Tests/Opcodes/FunctionTests.cs
@@ -0,0 +1,26 @@
+﻿using Gameboy.VM.CPU;
+using Xunit;
+
+namespace Gameboy.VM.Cpu.Tests.Opcodes
+{
+    public class FunctionTests
+    {
+        [Fact]
+        public void TestCallFunctionAndReturn()
+        {
+            var cpu = new CPU.CPU(new MMU(null, null, null));
+            var alu = new ALU(cpu);
+
+            cpu.Registers.StackPointer = 0xFFFE;
+            cpu.Registers.ProgramCounter = 0x0100;
+            alu.Call(0x0200);
+            Assert.Equal(0x0200, cpu.Registers.ProgramCounter);
+            Assert.Equal(0xFFFC, cpu.Registers.StackPointer);
+
+            alu.Return();
+
+            Assert.Equal(0x0100, cpu.Registers.ProgramCounter);
+            Assert.Equal(0xFFFE, cpu.Registers.StackPointer);
+        }
+    }
+}
