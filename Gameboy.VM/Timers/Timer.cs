53c53cde07e1d40f363b07fcd16dcf4fab4157f5
David Tyler <davet.code@gmail.com>
2020-01-11 20:17:26 +0000

    Moved timer states into separate file

--- Gameboy.VM/Timers/Timer.cs
+++ Gameboy.VM/Timers/Timer.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace Gameboy.VM.Timers
+﻿namespace Gameboy.VM.Timers
 {
     internal class Timer
     {
@@ -64,25 +62,10 @@ namespace Gameboy.VM.Timers
         internal byte TimerCounter { get; set; }
         internal byte TimerModulo { get; set; }
         #endregion
-    }
 
-    internal enum TimerClockSelect
-    {
-        f_2_10 = 0x00, // 4.096 KHz
-        f_2_4 = 0x01, // 262.144 KHz
-        f_2_6 = 0x02, // 65.536 KHz
-        f_2_8 = 0x03, // 16.384 KHz
-    }
-
-    internal static class TimerClockSelectExtensions
-    {
-        internal static int Step(this TimerClockSelect timerClockSelect) => timerClockSelect switch
+        public override string ToString()
         {
-            TimerClockSelect.f_2_10 => 1024,
-            TimerClockSelect.f_2_4 => 16,
-            TimerClockSelect.f_2_6 => 64,
-            TimerClockSelect.f_2_8 => 256,
-            _ => throw new ArgumentOutOfRangeException(nameof(timerClockSelect), timerClockSelect, "Unhandled TimerClockSelect value")
-        };
+            return $"DIV:{Divider:X2}, TAC:{TimerCounter}, TIM:{TimerModulo}, TC:{TimerController}";
+        }
     }
 }
---
+++ Gameboy.VM/Timers/TimerClockSelect.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Gameboy.VM.Timers
+{
+    internal enum TimerClockSelect
+    {
+        f_2_10 = 0x00, // 4.096 KHz
+        f_2_4 = 0x01, // 262.144 KHz
+        f_2_6 = 0x02, // 65.536 KHz
+        f_2_8 = 0x03, // 16.384 KHz
+    }
+
+    internal static class TimerClockSelectExtensions
+    {
+        internal static int Step(this TimerClockSelect timerClockSelect) => timerClockSelect switch
+        {
+            TimerClockSelect.f_2_10 => 1024,
+            TimerClockSelect.f_2_4 => 16,
+            TimerClockSelect.f_2_6 => 64,
+            TimerClockSelect.f_2_8 => 256,
+            _ => throw new ArgumentOutOfRangeException(nameof(timerClockSelect), timerClockSelect, "Unhandled TimerClockSelect value")
+        };
+    }
+}
