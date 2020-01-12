0b900f99bd11f13e98e87b8c2819c9c1db0bc767
David Tyler <davet.code@gmail.com>
2020-01-12 21:01:21 +0000

    Tested all BIT opcodes (CB40-CB7F)

--- Gameboy.VM.Cpu.Tests/CPU/AluRotShiftBitTests.cs
+++ Gameboy.VM.Cpu.Tests/CPU/AluRotShiftBitTests.cs
@@ -475,6 +475,163 @@ namespace Gameboy.VM.Tests.CPU
         }
 
         [Theory]
+        [InlineData(0x0, 0x40, 0x06, true)] // BIT 0,B - Set
+        [InlineData(0x0, 0x41, 0x0E, true)] // BIT 0,C - Set
+        [InlineData(0x0, 0x42, 0x16, true)] // BIT 0,D - Set
+        [InlineData(0x0, 0x43, 0x1E, true)] // BIT 0,E - Set
+        [InlineData(0x0, 0x44, 0x26, true)] // BIT 0,H - Set
+        [InlineData(0x0, 0x45, 0x2E, true)] // BIT 0,L - Set
+        [InlineData(0x0, 0x46, 0x36, true)] // BIT 0,(HL) - Set
+        [InlineData(0x0, 0x47, 0x3E, true)] // BIT 0,A - Set
+        [InlineData(0x1, 0x40, 0x06, false)] // BIT 0,B - Unset
+        [InlineData(0x1, 0x41, 0x0E, false)] // BIT 0,C - Unset
+        [InlineData(0x1, 0x42, 0x16, false)] // BIT 0,D - Unset
+        [InlineData(0x1, 0x43, 0x1E, false)] // BIT 0,E - Unset
+        [InlineData(0x1, 0x44, 0x26, false)] // BIT 0,H - Unset
+        [InlineData(0x1, 0x45, 0x2E, false)] // BIT 0,L - Unset
+        [InlineData(0x1, 0x46, 0x36, false)] // BIT 0,(HL) - Unset
+        [InlineData(0x1, 0x47, 0x3E, false)] // BIT 0,A - Unset
+
+        [InlineData(0x0, 0x48, 0x06, true)] // BIT 1,B - Set
+        [InlineData(0x0, 0x49, 0x0E, true)] // BIT 1,C - Set
+        [InlineData(0x0, 0x4A, 0x16, true)] // BIT 1,D - Set
+        [InlineData(0x0, 0x4B, 0x1E, true)] // BIT 1,E - Set
+        [InlineData(0x0, 0x4C, 0x26, true)] // BIT 1,H - Set
+        [InlineData(0x0, 0x4D, 0x2E, true)] // BIT 1,L - Set
+        [InlineData(0x0, 0x4E, 0x36, true)] // BIT 1,(HL) - Set
+        [InlineData(0x0, 0x4F, 0x3E, true)] // BIT 1,A - Set
+        [InlineData(0x2, 0x48, 0x06, false)] // BIT 1,B - Unset
+        [InlineData(0x2, 0x49, 0x0E, false)] // BIT 1,C - Unset
+        [InlineData(0x2, 0x4A, 0x16, false)] // BIT 1,D - Unset
+        [InlineData(0x2, 0x4B, 0x1E, false)] // BIT 1,E - Unset
+        [InlineData(0x2, 0x4C, 0x26, false)] // BIT 1,H - Unset
+        [InlineData(0x2, 0x4D, 0x2E, false)] // BIT 1,L - Unset
+        [InlineData(0x2, 0x4E, 0x36, false)] // BIT 1,(HL) - Unset
+        [InlineData(0x2, 0x4F, 0x3E, false)] // BIT 1,A - Unset
+
+        [InlineData(0x0, 0x50, 0x06, true)] // BIT 2,B - Set
+        [InlineData(0x0, 0x51, 0x0E, true)] // BIT 2,C - Set
+        [InlineData(0x0, 0x52, 0x16, true)] // BIT 2,D - Set
+        [InlineData(0x0, 0x53, 0x1E, true)] // BIT 2,E - Set
+        [InlineData(0x0, 0x54, 0x26, true)] // BIT 2,H - Set
+        [InlineData(0x0, 0x55, 0x2E, true)] // BIT 2,L - Set
+        [InlineData(0x0, 0x56, 0x36, true)] // BIT 2,(HL) - Set
+        [InlineData(0x0, 0x57, 0x3E, true)] // BIT 2,A - Set
+        [InlineData(0x4, 0x50, 0x06, false)] // BIT 2,B - Unset
+        [InlineData(0x4, 0x51, 0x0E, false)] // BIT 2,C - Unset
+        [InlineData(0x4, 0x52, 0x16, false)] // BIT 2,D - Unset
+        [InlineData(0x4, 0x53, 0x1E, false)] // BIT 2,E - Unset
+        [InlineData(0x4, 0x54, 0x26, false)] // BIT 2,H - Unset
+        [InlineData(0x4, 0x55, 0x2E, false)] // BIT 2,L - Unset
+        [InlineData(0x4, 0x56, 0x36, false)] // BIT 2,(HL) - Unset
+        [InlineData(0x4, 0x57, 0x3E, false)] // BIT 2,A - Unset
+
+        [InlineData(0x0, 0x58, 0x06, true)] // BIT 3,B - Set
+        [InlineData(0x0, 0x59, 0x0E, true)] // BIT 3,C - Set
+        [InlineData(0x0, 0x5A, 0x16, true)] // BIT 3,D - Set
+        [InlineData(0x0, 0x5B, 0x1E, true)] // BIT 3,E - Set
+        [InlineData(0x0, 0x5C, 0x26, true)] // BIT 3,H - Set
+        [InlineData(0x0, 0x5D, 0x2E, true)] // BIT 3,L - Set
+        [InlineData(0x0, 0x5E, 0x36, true)] // BIT 3,(HL) - Set
+        [InlineData(0x0, 0x5F, 0x3E, true)] // BIT 3,A - Set
+        [InlineData(0x8, 0x58, 0x06, false)] // BIT 3,B - Unset
+        [InlineData(0x8, 0x59, 0x0E, false)] // BIT 3,C - Unset
+        [InlineData(0x8, 0x5A, 0x16, false)] // BIT 3,D - Unset
+        [InlineData(0x8, 0x5B, 0x1E, false)] // BIT 3,E - Unset
+        [InlineData(0x8, 0x5C, 0x26, false)] // BIT 3,H - Unset
+        [InlineData(0x8, 0x5D, 0x2E, false)] // BIT 3,L - Unset
+        [InlineData(0x8, 0x5E, 0x36, false)] // BIT 3,(HL) - Unset
+        [InlineData(0x8, 0x5F, 0x3E, false)] // BIT 3,A - Unset
+
+        [InlineData(0x0, 0x60, 0x06, true)] // BIT 4,B - Set
+        [InlineData(0x0, 0x61, 0x0E, true)] // BIT 4,C - Set
+        [InlineData(0x0, 0x62, 0x16, true)] // BIT 4,D - Set
+        [InlineData(0x0, 0x63, 0x1E, true)] // BIT 4,E - Set
+        [InlineData(0x0, 0x64, 0x26, true)] // BIT 4,H - Set
+        [InlineData(0x0, 0x65, 0x2E, true)] // BIT 4,L - Set
+        [InlineData(0x0, 0x66, 0x36, true)] // BIT 4,(HL) - Set
+        [InlineData(0x0, 0x67, 0x3E, true)] // BIT 4,A - Set
+        [InlineData(0x10, 0x60, 0x06, false)] // BIT 4,B - Unset
+        [InlineData(0x10, 0x61, 0x0E, false)] // BIT 4,C - Unset
+        [InlineData(0x10, 0x62, 0x16, false)] // BIT 4,D - Unset
+        [InlineData(0x10, 0x63, 0x1E, false)] // BIT 4,E - Unset
+        [InlineData(0x10, 0x64, 0x26, false)] // BIT 4,H - Unset
+        [InlineData(0x10, 0x65, 0x2E, false)] // BIT 4,L - Unset
+        [InlineData(0x10, 0x66, 0x36, false)] // BIT 4,(HL) - Unset
+        [InlineData(0x10, 0x67, 0x3E, false)] // BIT 4,A - Unset
+
+        [InlineData(0x0, 0x68, 0x06, true)] // BIT 5,B - Set
+        [InlineData(0x0, 0x69, 0x0E, true)] // BIT 5,C - Set
+        [InlineData(0x0, 0x6A, 0x16, true)] // BIT 5,D - Set
+        [InlineData(0x0, 0x6B, 0x1E, true)] // BIT 5,E - Set
+        [InlineData(0x0, 0x6C, 0x26, true)] // BIT 5,H - Set
+        [InlineData(0x0, 0x6D, 0x2E, true)] // BIT 5,L - Set
+        [InlineData(0x0, 0x6E, 0x36, true)] // BIT 5,(HL) - Set
+        [InlineData(0x0, 0x6F, 0x3E, true)] // BIT 5,A - Set
+        [InlineData(0x20, 0x68, 0x06, false)] // BIT 5,B - Unset
+        [InlineData(0x20, 0x69, 0x0E, false)] // BIT 5,C - Unset
+        [InlineData(0x20, 0x6A, 0x16, false)] // BIT 5,D - Unset
+        [InlineData(0x20, 0x6B, 0x1E, false)] // BIT 5,E - Unset
+        [InlineData(0x20, 0x6C, 0x26, false)] // BIT 5,H - Unset
+        [InlineData(0x20, 0x6D, 0x2E, false)] // BIT 5,L - Unset
+        [InlineData(0x20, 0x6E, 0x36, false)] // BIT 5,(HL) - Unset
+        [InlineData(0x20, 0x6F, 0x3E, false)] // BIT 5,A - Unset
+
+        [InlineData(0x0, 0x70, 0x06, true)] // BIT 6,B - Set
+        [InlineData(0x0, 0x71, 0x0E, true)] // BIT 6,C - Set
+        [InlineData(0x0, 0x72, 0x16, true)] // BIT 6,D - Set
+        [InlineData(0x0, 0x73, 0x1E, true)] // BIT 6,E - Set
+        [InlineData(0x0, 0x74, 0x26, true)] // BIT 6,H - Set
+        [InlineData(0x0, 0x75, 0x2E, true)] // BIT 6,L - Set
+        [InlineData(0x0, 0x77, 0x36, true)] // BIT 6,(HL) - Set
+        [InlineData(0x0, 0x77, 0x3E, true)] // BIT 6,A - Set
+        [InlineData(0x40, 0x70, 0x06, false)] // BIT 6,B - Unset
+        [InlineData(0x40, 0x71, 0x0E, false)] // BIT 6,C - Unset
+        [InlineData(0x40, 0x72, 0x16, false)] // BIT 6,D - Unset
+        [InlineData(0x40, 0x73, 0x1E, false)] // BIT 6,E - Unset
+        [InlineData(0x40, 0x74, 0x26, false)] // BIT 6,H - Unset
+        [InlineData(0x40, 0x75, 0x2E, false)] // BIT 6,L - Unset
+        [InlineData(0x40, 0x76, 0x36, false)] // BIT 6,(HL) - Unset
+        [InlineData(0x40, 0x77, 0x3E, false)] // BIT 6,A - Unset
+
+        [InlineData(0x0, 0x78, 0x06, true)] // BIT 7,B - Set
+        [InlineData(0x0, 0x79, 0x0E, true)] // BIT 7,C - Set
+        [InlineData(0x0, 0x7A, 0x16, true)] // BIT 7,D - Set
+        [InlineData(0x0, 0x7B, 0x1E, true)] // BIT 7,E - Set
+        [InlineData(0x0, 0x7C, 0x26, true)] // BIT 7,H - Set
+        [InlineData(0x0, 0x7D, 0x2E, true)] // BIT 7,L - Set
+        [InlineData(0x0, 0x7E, 0x36, true)] // BIT 7,(HL) - Set
+        [InlineData(0x0, 0x7F, 0x3E, true)] // BIT 7,A - Set
+        [InlineData(0x80, 0x78, 0x06, false)] // BIT 7,B - Unset
+        [InlineData(0x80, 0x79, 0x0E, false)] // BIT 7,C - Unset
+        [InlineData(0x80, 0x7A, 0x16, false)] // BIT 7,D - Unset
+        [InlineData(0x80, 0x7B, 0x1E, false)] // BIT 7,E - Unset
+        [InlineData(0x80, 0x7C, 0x26, false)] // BIT 7,H - Unset
+        [InlineData(0x80, 0x7D, 0x2E, false)] // BIT 7,L - Unset
+        [InlineData(0x80, 0x7E, 0x36, false)] // BIT 7,(HL) - Unset
+        [InlineData(0x80, 0x7F, 0x3E, false)] // BIT 7,A - Unset
+        public void TestBIT(byte initialValue, byte bitOpcode, byte loadOpcode, bool z)
+        {
+            var device = TestUtils.CreateTestDevice(new byte[]
+            {
+                0x21, 0x00, 0xC0, // Load 0xC000 into HL
+                loadOpcode, initialValue, // Load into reg
+                0x37, // Set carry flag to 1
+                0xCB, bitOpcode, // BIT reg (opcode under test)
+            });
+
+            for (var ii = 0; ii < 6; ii++) // 2 to get to PC 0x150 then 4 to setup and act
+            {
+                device.Step();
+            }
+
+            Assert.True(device.CPU.Registers.GetFlag(CpuFlags.CarryFlag)); // Test must not touch it
+            Assert.True(device.CPU.Registers.GetFlag(CpuFlags.HalfCarryFlag)); // Half carry always set to true by BIT operation
+            Assert.False(device.CPU.Registers.GetFlag(CpuFlags.SubtractFlag)); // Subtract flag always set to false by BIT operation
+            Assert.Equal(z, device.CPU.Registers.GetFlag(CpuFlags.ZeroFlag));
+        }
+
+        [Theory]
         [InlineData(0x80, 7, false)]
         [InlineData(0xEF, 4, true)]
         [InlineData(0xFE, 0, true)]
