445a74ffcb3209574fb0bd644a1edee32ceb729b
David Tyler <david.tyler@metaswitch.com>
2020-01-15 13:58:44 +0000

    Set correct default values of sprites so they start hidden

--- Gameboy.VM/LCD/Sprite.cs
+++ Gameboy.VM/LCD/Sprite.cs
@@ -1,23 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Gameboy.VM.LCD
+﻿namespace Gameboy.VM.LCD
 {
     internal class Sprite
     {
         internal SpriteToBgPriority SpriteToBgPriority { get; set; }
 
-        internal int Y { get; set; }
+        internal int Y { get; set; } = -16;
 
-        internal int X { get; set; }
+        internal int X { get; set; } = -8;
 
-        internal int TileNumber { get; set; }
+        internal int TileNumber { get; set; } = 0;
 
-        internal bool YFlip { get; set; }
+        internal bool YFlip { get; set; } = false;
 
-        internal bool XFlip { get; set; }
+        internal bool XFlip { get; set; } = false;
 
-        internal bool UsePalette1 { get; set; }
+        internal bool UsePalette1 { get; set; } = false;
     }
 }
