9f691baaec71c5575805a22d884b668e7e3d1a8e
David Tyler <david.tyler@metaswitch.com>
2020-01-15 13:57:57 +0000

    Fix for DMA not transferring the correct amount of memory

--- Gameboy.VM/DMAController.cs
+++ Gameboy.VM/DMAController.cs
@@ -19,7 +19,7 @@
         {
             var address = dataAddress << 8;
 
-            for (var ii = 0; ii < 0x0A; ii++)
+            for (var ii = 0; ii < 0x100; ii++)
             {
                 _device.MMU.WriteByte((ushort)(0xFE00 + ii), _device.MMU.ReadByte((ushort)(address + ii)));
             }
