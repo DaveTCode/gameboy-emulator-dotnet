6c17716c4dc400ced32407b6949c70b9cab9c9db
David Tyler <david.tyler@metaswitch.com>
2020-01-18 16:33:33 +0000

    Fix broken window preventing zelda from displaying correctly

--- Gameboy.VM/LCD/LCDDriver.cs
+++ Gameboy.VM/LCD/LCDDriver.cs
@@ -234,7 +234,7 @@ namespace Gameboy.VM.LCD
                 // Determine the x position relative to whether we're in the window or the background
                 // taking into account scrolling.
                 var xPos = (UsingWindowForScanline && pixel >= _device.LCDRegisters.WindowX) ?
-                    ((pixel - _device.LCDRegisters.WindowX) & 0xFF) :
+                    ((pixel - _device.LCDRegisters.WindowX + 7) & 0xFF) :
                     ((pixel + _device.LCDRegisters.ScrollX) & 0xFF);
 
                 var tileCol = xPos / 8;
@@ -320,7 +320,7 @@ namespace Gameboy.VM.LCD
 
         #region Utility functions on registers
 
-        private bool UsingWindowForScanline => _device.LCDRegisters.IsWindowEnabled && _device.LCDRegisters.LCDCurrentScanline <= _device.LCDRegisters.WindowY;
+        private bool UsingWindowForScanline => _device.LCDRegisters.IsWindowEnabled && _device.LCDRegisters.LCDCurrentScanline >= _device.LCDRegisters.WindowY;
 
         private ushort GetTileDataAddress(byte tileNumber)
         {
