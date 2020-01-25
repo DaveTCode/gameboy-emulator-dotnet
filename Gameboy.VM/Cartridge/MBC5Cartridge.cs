8eed87f7dab61288dae9a120c5566bbdcc4e2eac
David Tyler <david.tyler@metaswitch.com>
2020-01-25 21:55:45 +0000

    Small fix for MBC5 cartridges with no RAM that attempt to use RAM Bank switching

--- Gameboy.VM/Cartridge/MBC5Cartridge.cs
+++ Gameboy.VM/Cartridge/MBC5Cartridge.cs
@@ -2,8 +2,8 @@
 {
     internal class MBC5Cartridge : Cartridge
     {
-        private byte _ROMB0; // Lower ROM Bank register
-        private byte _ROMB1; // Upper ROM Bank register
+        private byte _romb0; // Lower ROM Bank register
+        private byte _romb1; // Upper ROM Bank register
 
         public MBC5Cartridge(byte[] contents) : base(contents)
         {
@@ -17,17 +17,18 @@
             }
             else if (address >= 0x2000 && address <= 0x2FFF)
             {
-                _ROMB0 = value;
-                RomBank = ((_ROMB1 << 8) | _ROMB0) % ROMSize.NumberBanks();
+                _romb0 = value;
+                RomBank = ((_romb1 << 8) | _romb0) % ROMSize.NumberBanks();
             }
             else if (address >= 0x3000 && address <= 0x3FFF)
             {
-                _ROMB1 = (byte)(value & 0b00000001); // Only bottom 1 bit is used AFAICT
-                RomBank = ((_ROMB1 << 8) | _ROMB0) % ROMSize.NumberBanks();
+                _romb1 = (byte)(value & 0b00000001); // Only bottom 1 bit is used AFAICT
+                RomBank = ((_romb1 << 8) | _romb0) % ROMSize.NumberBanks();
             }
             else if (address >= 0x4000 && address <= 0x5FFF)
             {
-                RamBank = (value & 8) % RAMSize.NumberBanks();
+                if (RAMSize == CartridgeRAMSize.None) RamBank = 0;
+                else RamBank = (value & 8) % RAMSize.NumberBanks();
             }
         }
     }
