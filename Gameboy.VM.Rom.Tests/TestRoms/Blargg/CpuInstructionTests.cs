689fdb3100ede0d295b0a73f2a2bf1f368aa327a
David Tyler <david.tyler@metaswitch.com>
2020-02-12 14:20:34 +0000

    Added test case for blargg instruction timings

--- Gameboy.VM.Rom.Tests/TestRoms/Blargg/CpuInstructionTests.cs
+++ Gameboy.VM.Rom.Tests/TestRoms/Blargg/CpuInstructionTests.cs
@@ -7,10 +7,22 @@ namespace Gameboy.VM.Rom.Tests.TestRoms.Blargg
     public class CpuInstructionTests
     {
         [Fact(DisplayName = "Tests all of the CPU instructions thoroughly")]
-        public async Task TestCpuInstructionsRom()
+        public async Task TestCpuInstructions()
         {
-            var expectedFrameBuffer = await File.ReadAllLinesAsync(Path.Join(TestUtils.SolutionDirectory, "Roms", "tests", "blargg", "cpu_instrs", "framebuffer"));
-            await TestUtils.TestRomAgainstResult(Path.Join("Roms", "tests", "blargg", "cpu_instrs", "cpu_instrs.gb"), expectedFrameBuffer, 1000 * 120, 0x6F1);
+            var expectedFrameBuffer = await File.ReadAllLinesAsync(
+                Path.Join(TestUtils.SolutionDirectory, "Roms", "tests", "blargg", "cpu_instrs", "framebuffer"));
+            await TestUtils.TestRomAgainstResult(
+                Path.Join("Roms", "tests", "blargg", "cpu_instrs", "cpu_instrs.gb"), expectedFrameBuffer, 1000 * 120, 0x6F1);
         }
+
+        [Fact(DisplayName = "Test each instruction takes the correct number of t-cycles")]
+        public async Task TestCpuInstructionTiming()
+        {
+            var expectedFrameBuffer = await File.ReadAllLinesAsync(
+                Path.Join(TestUtils.SolutionDirectory, "Roms", "tests", "blargg", "instr_timing", "framebuffer"));
+            await TestUtils.TestRomAgainstResult(
+                Path.Join("Roms", "tests", "blargg", "instr_timing", "instr_timing.gb"), expectedFrameBuffer, 1000 * 120, 0xC8B0);
+        }
+
     }
 }
