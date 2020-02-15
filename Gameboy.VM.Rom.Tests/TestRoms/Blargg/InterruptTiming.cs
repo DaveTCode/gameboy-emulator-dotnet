e729e206b80b2723bc6f6ed4995131c8ae5385d9
David Tyler <davet.code@gmail.com>
2020-02-15 13:03:41 +0000

    Added interrupt timing test but marked as skipped until we implement double speed mode

---
+++ Gameboy.VM.Rom.Tests/TestRoms/Blargg/InterruptTiming.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Gameboy.VM.Rom.Tests.TestRoms.Blargg
+{
+    public class InterruptTiming
+    {
+        [Fact(DisplayName = "Test OAM bug behavior", Skip = "TODO - Double speed not yet implemented")]
+        public async Task TestOAMBugBehavior()
+        {
+            var expectedFrameBuffer = await File.ReadAllLinesAsync(
+                Path.Join(TestUtils.SolutionDirectory, "Roms", "tests", "blargg", "interrupt_time", "framebuffer"));
+            await TestUtils.TestRomAgainstResult(
+                Path.Join("Roms", "tests", "blargg", "interrupt_time", "interrupt_time.gb"), expectedFrameBuffer, 1000 * 120, 0xC9C9);
+        }
+    }
+}
