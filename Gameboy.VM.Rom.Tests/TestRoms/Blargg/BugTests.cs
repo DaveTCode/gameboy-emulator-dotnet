0e24b284becf6700c27b179a9f1f8016fb83d488
David Tyler <davet.code@gmail.com>
2020-02-15 12:54:48 +0000

    OAM scribble bug added although test currently ignored since it's not yet implemented

--- Gameboy.VM.Rom.Tests/TestRoms/Blargg/BugTests.cs
+++ Gameboy.VM.Rom.Tests/TestRoms/Blargg/BugTests.cs
@@ -12,7 +12,16 @@ namespace Gameboy.VM.Rom.Tests.TestRoms.Blargg
             var expectedFrameBuffer = await File.ReadAllLinesAsync(
                 Path.Join(TestUtils.SolutionDirectory, "Roms", "tests", "blargg", "halt_bug_framebuffer"));
             await TestUtils.TestRomAgainstResult(
-                Path.Join("Roms", "tests", "blargg", "halt_bug.gb"), expectedFrameBuffer, 1000 * 120, 0x06F1);
+                Path.Join("Roms", "tests", "blargg", "halt_bug.gb"), expectedFrameBuffer, 1000 * 120, 0xC818);
+        }
+
+        [Fact(DisplayName = "Test OAM bug behavior", Skip = "OAM bug not implemented at the moment")]
+        public async Task TestOAMBugBehavior()
+        {
+            var expectedFrameBuffer = await File.ReadAllLinesAsync(
+                Path.Join(TestUtils.SolutionDirectory, "Roms", "tests", "blargg", "oam_bug", "framebuffer"));
+            await TestUtils.TestRomAgainstResult(
+                Path.Join("Roms", "tests", "blargg", "oam_bug", "oam_bug.gb"), expectedFrameBuffer, 1000 * 120, 0x2BD2);
         }
     }
 }
